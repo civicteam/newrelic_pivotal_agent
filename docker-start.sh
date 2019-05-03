90682edc4e258fc831a06fd0522273f27ca1b95b
christiansaiki <christian@revmob.com>
2019-05-03 16:51:52 -0300

    fixed docker start script

--- docker-start.sh
+++ docker-start.sh
@@ -4,7 +4,7 @@ set -e
 set -u
 
 # ensure that volumes were set and the dependencies started before running the application
-./scripts/wait-dir.sh ${NODE_CONFIG_DIR}
+./wait-dir.sh ${NODE_CONFIG_DIR}
 
 source ${NODE_CONFIG_DIR}/${NODE_ENV}
 export $(cut -d= -f1 -f2 ${NODE_CONFIG_DIR}/${NODE_ENV})
@@ -12,4 +12,4 @@ export $(cut -d= -f1 -f2 ${NODE_CONFIG_DIR}/${NODE_ENV})
 source ${NODE_CONFIG_DIR}/secrets
 export $(cut -d= -f1 -f2 ${NODE_CONFIG_DIR}/secrets)
 
-./start.sh
+./start.sh 
